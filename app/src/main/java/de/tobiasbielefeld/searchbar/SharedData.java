2019183afb22122cc8e348fbd7a490e59a1d8ed2
Josiah Ulfers <ju@josiahulfers.com>
2018-03-16 20:17:02 -0400

    DuckDuckGo is actually entry 6

--- app/src/main/java/de/tobiasbielefeld/searchbar/SharedData.java
+++ app/src/main/java/de/tobiasbielefeld/searchbar/SharedData.java
@@ -67,7 +67,7 @@ public class SharedData {
             PREF_STATUS_BAR = res.getString(R.string.pref_key_hide_status_bar);
             PREF_LANGUAGE = res.getString(R.string.pref_key_language);
 
-            DEFAULT_SEARCH_URL = res.getStringArray(R.array.search_engine_uris)[5];
+            DEFAULT_SEARCH_URL = res.getStringArray(R.array.search_engine_uris)[6];
             DEFAULT_ORIENTATION = res.getStringArray(R.array.pref_orientation_values)[0];
             DEFAULT_STATUS_BAR = res.getBoolean(R.bool.default_status_bar);
         }
